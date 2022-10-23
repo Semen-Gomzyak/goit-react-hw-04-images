d596be2e8fb74632ac0ef4e82b0545a5a68f591f
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-10-23 11:27:50 +0200

    delete useLayoutEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState, useEffect } from 'react';
+import React, {  useState, useEffect } from 'react'; //вернуть useLayoutEffect
 import { ToastContainer, toast } from 'react-toastify';
 import { AppContainer } from './App.styled';
 import { SearchbarForm } from './Searchbar/Searchbar';
